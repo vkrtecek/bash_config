2c20f72a310b906f75f657357cb6879171229050
Vojtěch Stuchlík <vojtech.stuchlik@blogic.cz>
2017-09-08 13:50:25 +0200

    fix dollar coloring on error

--- .prompt.sh
+++ .prompt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+RET_VAL=$?
 
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWSTASHSTATE=1
@@ -7,16 +8,13 @@ export GIT_PS1_SHOWUPSTREAM="auto verbose"
 # Do promˇenn ́e PS1 pˇridat: $(__git_ps1 " [%s]")
 
 
-
-S=$?
-
 END="\[\e[0m\]"
 CYAN="\[\e[36;1m\]"
 GREEN="\[\e[32;1m\]"
 YELLOW="\[\e[33;1m\]"
 RED="\[\e[1;31m\]"
 
-if [ $S -eq 0 ]
+if [ $RET_VAL -eq 0 ]
 then
   DOLLAR="${YELLOW}\$"
 else
